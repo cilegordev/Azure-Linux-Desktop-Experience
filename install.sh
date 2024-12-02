aeec991e8f0afcdea7620e0c28ac4e2a14e01d33
cilegordev <123652808+cilegordev@users.noreply.github.com>
2024-12-02 23:55:03 +0700

    update #9 xfce4.20pre2 road to wayland

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 #
 # this script will automatically compile xfce4
 # warning don't run as sudo ./install.sh
-# exluded libxfce4util & xfconf cause pre installed on repo
 # time and date will set to Indonesia/Jakarta, if you need to use others, you can adjust it after compile
 # sudo with asking password will be gone, stop washing your time!
 # i don't use vim, vim will be replaced with nano
@@ -65,25 +64,36 @@ sudo dnf -y remove vim
 git clone https://gitlab.freedesktop.org/xorg/driver/xf86-video-fbdev && cd xf86-video-fbdev && ./autogen.sh && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
 git clone https://gitlab.freedesktop.org/xorg/driver/xf86-video-vesa && cd xf86-video-vesa && ./autogen.sh && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
 #xfce4-component!
+git clone https://github.com/mesonbuild/meson && cd meson && chmod +x setup.py && sudo python3 setup.py install && cd ~/pre
 cd Azure-Linux-Desktop-Experience/gtk-layer-shell && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && sudo ldconfig && cd ~/pre
-wget https://archive.xfce.org/src/xfce/libxfce4ui/4.19/libxfce4ui-4.19.3.tar.bz2 && tar -xvf libxfce4ui-4.19.3.tar.bz2 && cd libxfce4ui-4.19.3 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/exo/4.19/exo-4.19.0.tar.bz2 && tar -xvf exo-4.19.0.tar.bz2 && cd exo-4.19.0 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/garcon/4.19/garcon-4.19.0.tar.bz2 && tar -xvf garcon-4.19.0.tar.bz2 && cd garcon-4.19.0 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://download.gnome.org/sources/libwnck/43/libwnck-43.1.tar.xz && tar -xvf libwnck-43.1.tar.xz && cd libwnck-43.1 && mkdir build && cd build && meson setup .. --prefix=/usr && sudo ninja install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfce4-dev-tools/4.19/xfce4-dev-tools-4.19.0.tar.bz2 && tar -xvf xfce4-dev-tools-4.19.0.tar.bz2 && cd xfce4-dev-tools-4.19.0 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/libxfce4windowing/4.19/libxfce4windowing-4.19.2.tar.bz2 && tar -xvf libxfce4windowing-4.19.2.tar.bz2 && cd libxfce4windowing-4.19.2 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfce4-panel/4.19/xfce4-panel-4.19.2.tar.bz2 && tar -xvf xfce4-panel-4.19.2.tar.bz2 && cd xfce4-panel-4.19.2 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/apps/xfce4-panel-profiles/1.0/xfce4-panel-profiles-1.0.14.tar.bz2 && tar -xvf xfce4-panel-profiles-1.0.14.tar.bz2 && cd xfce4-panel-profiles-1.0.14 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://download.gnome.org/sources/gsettings-desktop-schemas/47/gsettings-desktop-schemas-47.tar.xz && tar -xvf gsettings-desktop-schemas-47.tar.xz && cd gsettings-desktop-schemas-47 && mkdir build && cd build && meson setup .. --prefix=/usr && sudo ninja install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/thunar/4.19/thunar-4.19.2.tar.bz2 && tar -xvf thunar-4.19.2.tar.bz2 && cd thunar-4.19.2 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/thunar-volman/4.18/thunar-volman-4.18.0.tar.bz2 && tar -xvf thunar-volman-4.18.0.tar.bz2 && cd thunar-volman-4.18.0 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/tumbler/4.19/tumbler-4.19.0.tar.bz2 && tar -xvf tumbler-4.19.0.tar.bz2 && cd tumbler-4.19.0 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfce4-appfinder/4.18/xfce4-appfinder-4.18.1.tar.bz2 && tar -xvf xfce4-appfinder-4.18.1.tar.bz2 && cd xfce4-appfinder-4.18.1 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfce4-power-manager/4.19/xfce4-power-manager-4.19.2.tar.bz2 && tar -xvf xfce4-power-manager-4.19.2.tar.bz2 && cd xfce4-power-manager-4.19.2 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfce4-settings/4.19/xfce4-settings-4.19.1.tar.bz2 && tar -xvf xfce4-settings-4.19.1.tar.bz2 && cd xfce4-settings-4.19.1 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfdesktop/4.19/xfdesktop-4.19.0.tar.bz2 && tar -xvf xfdesktop-4.19.0.tar.bz2 && cd xfdesktop-4.19.0 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfwm4/4.18/xfwm4-4.18.0.tar.bz2 && tar -xvf xfwm4-4.18.0.tar.bz2  && cd xfwm4-4.18.0 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
-wget https://archive.xfce.org/src/xfce/xfce4-session/4.19/xfce4-session-4.19.1.tar.bz2 && tar -xvf xfce4-session-4.19.1.tar.bz2 && cd xfce4-session-4.19.1 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+git clone https://gitlab.freedesktop.org/wayland/wayland && cd wayland && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+git clone https://gitlab.freedesktop.org/wayland/wayland-protocols && cd wayland-protocols && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+git clone https://gitlab.freedesktop.org/wayland/wayland-utils && cd wayland-utils && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+git clone https://gitlab.freedesktop.org/emersion/libdisplay-info && cd libdisplay-info && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+git clone https://gitlab.freedesktop.org/emersion/libliftoff && cd libliftoff && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+git clone https://github.com/kennylevinsen/seatd && cd seatd && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+git clone https://gitlab.freedesktop.org/wlroots/wlroots && cd wlroots && git checkout 0.18 && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+git clone https://github.com/labwc/labwc && cd labwc && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/libxfce4util-4.19.5.tar.bz2 && tar -xf libxfce4util-4.19.5.tar.bz2  && cd libxfce4util-4.19.5 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfconf-4.19.5.tar.bz2 && tar -xf xfconf-4.19.5.tar.bz2 && cd xfconf-4.19.5 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/libxfce4ui-4.19.7.tar.bz2 && tar -xf libxfce4ui-4.19.7.tar.bz2 && cd libxfce4ui-4.19.7 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/exo-4.19.2.tar.bz2 && tar -xf exo-4.19.2.tar.bz2 && cd exo-4.19.2 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/garcon-4.19.3.tar.bz2 && tar -xf garcon-4.19.3.tar.bz2 && cd garcon-4.19.3 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://download.gnome.org/sources/libwnck/43/libwnck-43.1.tar.xz && tar -xf libwnck-43.1.tar.xz && cd libwnck-43.1 && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfce4-dev-tools-4.19.4.tar.bz2 && tar -xf xfce4-dev-tools-4.19.4.tar.bz2 && cd xfce4-dev-tools-4.19.4 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/libxfce4windowing-4.19.10.tar.bz2 && tar -xf libxfce4windowing-4.19.10.tar.bz2 && cd libxfce4windowing-4.19.10 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfce4-panel-4.19.7.tar.bz2 && tar -xf xfce4-panel-4.19.7.tar.bz2 && cd xfce4-panel-4.19.7 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre 
+wget https://archive.xfce.org/src/apps/xfce4-panel-profiles/1.0/xfce4-panel-profiles-1.0.14.tar.bz2 && tar -xf xfce4-panel-profiles-1.0.14.tar.bz2 && cd xfce4-panel-profiles-1.0.14 && ./configure --prefix=/usr && sudo make install && cd ~/pre 
+wget https://download.gnome.org/sources/gsettings-desktop-schemas/47/gsettings-desktop-schemas-47.tar.xz && tar -xf gsettings-desktop-schemas-47.tar.xz && cd gsettings-desktop-schemas-47 && sed -i -r 's:"(/system):"/org/gnome\1:g' schemas/*.in && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release && sudo ninja install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/thunar-4.19.5.tar.bz2 && tar -xf thunar-4.19.5.tar.bz2 && cd thunar-4.19.5 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && sudo ldconfig && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/thunar-volman-4.19.1.tar.bz2 && tar -xf thunar-volman-4.19.1.tar.bz2 && cd thunar-volman-4.19.1 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/tumbler-4.19.3.tar.bz2 && tar -xf tumbler-4.19.3.tar.bz2 && cd tumbler-4.19.3 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre 
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfce4-appfinder-4.19.4.tar.bz2 && tar -xf xfce4-appfinder-4.19.4.tar.bz2 && cd xfce4-appfinder-4.19.4 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfce4-power-manager-4.19.5.tar.bz2 && tar -xf xfce4-power-manager-4.19.5.tar.bz2 && cd xfce4-power-manager-4.19.5 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfce4-settings-4.19.4.tar.bz2 && tar -xf xfce4-settings-4.19.4.tar.bz2 && cd xfce4-settings-4.19.4 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfdesktop-4.19.7.tar.bz2 && tar -xf xfdesktop-4.19.7.tar.bz2 && cd xfdesktop-4.19.7 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfwm4-4.19.1.tar.bz2 && tar -xf xfwm4-4.19.1.tar.bz2 && cd xfwm4-4.19.1 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
+wget https://archive.xfce.org/xfce/4.20pre2/src/xfce4-session-4.19.4.tar.bz2 && tar -xf xfce4-session-4.19.4.tar.bz2 && cd xfce4-session-4.19.4 && ./configure --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
 #xfce4-apps!
 wget https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.24.9.tar.xz && tar -xvf gstreamer-1.24.9.tar.xz && cd gstreamer-1.24.9 && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release -D gst_debug=false && sudo ninja install  && cd ~/pre
 wget https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-1.24.9.tar.xz && tar -xvf gst-plugins-base-1.24.9.tar.xz && cd gst-plugins-base-1.24.9 && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release --wrap-mode=nodownload && sudo ninja install && cd ~/pre
@@ -118,9 +128,9 @@ wget https://download.gnome.org/sources/NetworkManager/1.51/NetworkManager-1.51.
 wget https://download.gnome.org/sources/libnma/1.10/libnma-1.10.6.tar.xz && tar -xvf libnma-1.10.6.tar.xz && cd libnma-1.10.6 && ./autogen.sh --prefix=/usr --sysconfdir=/etc && sudo make install && cd ~/pre
 wget https://download.gnome.org/sources/network-manager-applet/1.36/network-manager-applet-1.36.0.tar.xz && tar -xvf network-manager-applet-1.36.0.tar.xz && cd network-manager-applet-1.36.0 && mkdir build && cd build && meson setup .. --prefix=/usr --sysconfdir=/etc --buildtype=release -D appindicator=no -D wwan=false && sudo ninja install && cd ~/pre
 wget https://ftp.mozilla.org/pub/firefox/releases/133.0/linux-x86_64/id/firefox-133.0.tar.bz2 && tar -xvf firefox-133.0.tar.bz2 && sudo mv -v firefox /opt && sudo ln -sv /opt/firefox/firefox /bin && sudo ln -sv /opt/firefox/firefox-bin /bin/mozilla-firefox && cd ~/pre
-#setup-x11!
+#setup-wayland!
 echo -e "XTerm*mainMenu: true \nXTerm*ToolBar: true \nXTerm*Background: black \nXTerm*Foreground: white" | tee -a ~/.Xdefaults
-echo -e "pulseaudio --start \nexec dbus-launch startxfce4" | tee -a ~/.xinitrc
+echo -e "pulseaudio --start \nexec dbus-launch startxfce4 --wayland" | tee -a ~/.xinitrc
 echo -e 'if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then\n    startx\nfi' >> ~/.bash_profile
 echo -e 'if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then\n    startx\nfi' >> ~/.zprofile
 chsh -s $(which zsh)
